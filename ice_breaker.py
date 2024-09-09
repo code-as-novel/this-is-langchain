19ae2e01ad96bfa4270ace2fc967bfd5c2554b5b
김상수 총괄 CONSULTANT/글로벌딜리버리추진팀 <sskim06@lgcns.com>
2024-09-09 09:17:53 +0900

    lecture 13

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,12 +4,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 
-
-information = """
-Elon Reeve Musk FRS (/ˈiːlɒn/; born June 28, 1971) is a businessman and investor known for his key roles in the space company SpaceX and the automotive company Tesla, Inc. Other involvements include ownership of X Corp., the company that operates the social media platform X (formerly known as Twitter), and his role in the founding of The Boring Company, xAI, Neuralink, and OpenAI. He is one of the wealthiest individuals in the world; as of August 2024 Forbes estimates his net worth to be US$247 billion.[3]
-
-Musk was born in Pretoria to Maye (née Haldeman), a model, and Errol Musk, a businessman and engineer. Musk briefly attended the University of Pretoria before immigrating to Canada at the age of 18, acquiring citizenship through his Canadian-born mother. Two years later he matriculated at Queen's University at Kingston in Canada. Musk later transferred to the University of Pennsylvania and received bachelor's degrees in economics and physics. He moved to California in 1995 to attend Stanford University, but dropped out after two days and, with his brother Kimbal, co-founded the online city guide software company Zip2. The startup was acquired by Compaq for $307 million in 1999. That same year Musk co-founded X.com, a direct bank. X.com merged with Confinity in 2000 to form PayPal. In October 2002, eBay acquired PayPal for $1.5 billion. Using $100 million of the money he made from the sale of PayPal, Musk founded SpaceX, a spaceflight services company, in 2002.
-"""
+from third_parties.linkedin import scrape_linkedin_profile
 
 if __name__ == '__main__':
     load_dotenv()
@@ -29,6 +24,7 @@ if __name__ == '__main__':
     llm = ChatOllama(model="llama3.1")
 
     chain = summary_prompt_template | llm | StrOutputParser()
-    res = chain.invoke(input={"information": information})
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/eden-marco/", mock=True)
+    res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
